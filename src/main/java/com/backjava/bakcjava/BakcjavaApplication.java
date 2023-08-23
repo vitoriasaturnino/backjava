799a398ef8b21563bdd05882614be0308ee3d04a
vitoriasaturnino <vim.saturnino@gmail.com>
2023-08-23 19:24:41 -0300

    build: initial commit

---
+++ src/main/java/com/backjava/bakcjava/BakcjavaApplication.java
@@ -0,0 +1,13 @@
+package com.backjava.bakcjava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BakcjavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BakcjavaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/backjava/bakcjava/BakcjavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.backjava.bakcjava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BakcjavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
